8d9e32fda9f2e51e78a396ada02d2d944f0da48b
Liam Hohertz <73028237+Liamx220@users.noreply.github.com>
2021-02-19 16:57:17 -0500

    Added movement and shooting.

---
+++ MiniJam74Chaos/Assets/scripts/movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed = 1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(new Vector3(0, 0, 20 * speed * Time.deltaTime));
+        }
+        if (Input.GetKey("x"))
+        {
+            transform.Rotate(new Vector3(0, 0, -20 * speed * Time.deltaTime));
+        }
+        
+    }
+}
---
+++ MiniJam74Chaos/Assets/shooting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public float bulletForce;
+    public float timeBetweenShots = 0.3333f;
+    private float timestamp;
+    void Update()
+    {
+        if (Time.time >= timestamp && Input.GetKeyDown("space"))
+        {
+            Shoot();
+        }
+
+        void Shoot()
+        {
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+            timestamp = Time.time + timeBetweenShots;
+        }
+    }
+
+}
